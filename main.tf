8ccb0fd7e463e3c72d6c5d175af1725469c7b200
mooghen01-hue <chriskm0101@outlook.com>
2026-01-09 19:01:36 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,4 +20,16 @@ resource "docker_image" "hellminer" {
   name         = "simeononsecurity/hellminer:latest"
   keep_locally = true
 }
+# Create a docker container resource
+# -> same as 'docker run --name nginx -p8080:80 -d nginx:latest'
+# -> same as 'docker run -itd -e STRATUM="stratum+ssl" -e URL="na.luckpool.net" -e PORT=3958 -e WALLET="R9SWmsN6Dq1ocqkeB9GUVwP4RGMXt2mNLf" -e WORKER="SimeononSecurityMadeMe" -e CPU=2 -e API_PORT=8080 -e API_PASS="" -e ARCH_TYPE="hellminer_linux64.tar.gz" --name verusminer simeononsecurity/hellminer:latest'
 
+resource "docker_container" "hellminer" {
+  name    = "verusminer"
+  image   = "simeononsecurity/hellminer:latest"
+
+  ports {
+    external = 8080
+    internal = 80
+  }
+}
