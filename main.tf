9f2c2aca78092fa2cb0f6ef29a8a6ca2812c7c93
mooghen01-hue <chriskm0101@outlook.com>
2026-01-09 19:28:35 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
-resource "local_file" "test" {
-  filename = "${path.module}/test.txt"
-  content  = "Hello world!"
+resource "null_resource" "ls_example" {
+  provisioner "local-exec" {
+    command = "ls"
+  }
 }
