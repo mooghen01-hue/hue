0041bb0ad386ac130287d21439924c066f0b3bd2
mooghen01-hue <chriskm0101@outlook.com>
2026-01-15 09:58:21 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,3 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 3.0"
-    }
-  }
-}
-provider "docker" {
-  host = "unix:///var/run/docker.sock"
-}
-
-resource "docker_image" "app" {
-  name = "chriskm12/system-service:latest"
-}
-
-resource "docker_container" "app" {
-  name  = "system-service"
-  image = docker_image.app.image_id
+resource "docker_image" "ubuntu" {
+  name = "ubuntu:precise"
 }
