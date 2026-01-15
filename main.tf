ab8c4045d95ccdf3dbdd5a0e257c41abbf84eb8b
mooghen01-hue <chriskm0101@outlook.com>
2026-01-15 09:30:48 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 3.0"
+      version = "3.9.0"
     }
   }
 }
@@ -14,7 +14,8 @@ provider "docker" {
 }
 
 resource "docker_image" "app" {
-  name = "chriskm12/system-service:latest"
+  name = "nginx:latest"
+#  name = "chriskm12/system-service:latest"
   keep_locally = true
 }
 
