f07723b142e18e50dc998c9f80a0928a2da11cbc
mooghen01-hue <chriskm0101@outlook.com>
2026-01-15 09:44:29 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,12 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 3.0"
-    }
-  }
-}
-
 provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
 
-resource "null_resource" "pull_image" {
-  provisioner "local-exec" {
-    command = "docker pull chriskm12/system-service:latest"
-  }
+resource "docker_image" "app" {
+  name = "chriskm12/system-service:latest"
+}
+
+resource "docker_container" "app" {
+  name  = "system-service"
+  image = docker_image.app.image_id
 }
