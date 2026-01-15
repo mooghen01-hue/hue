9d383c1f1e254c0d95c6ea26cd3589a0a956d376
mooghen01-hue <chriskm0101@outlook.com>
2026-01-15 08:42:25 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,31 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_image" "app" {
+  name = "chriskm12/system-service:latest"
+}
+
+resource "docker_container" "app" {
+  name  = "system-service"
+  image = docker_image.app.image_id
+
+  restart = "no"
+
+  resources {
+    cpu_shares = 512
+    memory     = 512
+  }
+}
 resource "null_resource" "run_1" {
   provisioner "local-exec" {
     command = <<EOT
